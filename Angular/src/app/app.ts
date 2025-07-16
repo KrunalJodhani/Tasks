4a8af57c1a9aaa5ffaa2ae72289e36ccba96d900
krunal.jodhani <krunal.jodhani@zeuslearning.com>
2025-07-16 20:00:36 +0530

    to-do update

--- Angular/src/app/app.ts
+++ Angular/src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core'
+import { Todo } from './todo';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule,CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('Angular');
+  todoVal: string = '';
+  list: Todo[] = [];
+
+  ngOnInit() {
+    this.list = [];
+    this.todoVal = '';
+  }
+
+  addTodo() {
+    if (this.todoVal !== '') {
+      const newTodo: Todo = {
+        id: this.list.length,
+        task: this.todoVal,
+        isDone: false,
+      }
+      this.list.push(newTodo);
+    }    
+    this.todoVal = '';
+  } 
+
+  deleteTodo(id: number) {
+    const todoToDelete = this.list.find(todo => todo.id === id);
+    
+    if (todoToDelete && todoToDelete.isDone) {
+      this.list = this.list.filter(todo => todo.id !== id);
+    } else {
+      alert("Please complete task before deleting it.");
+    }
+  }
 }
