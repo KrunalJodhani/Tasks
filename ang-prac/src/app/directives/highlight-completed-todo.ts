7e92ea103323de4a55984af00f858b04f724a784
Krunal Jodhani <21ce045@charusat.edu.in>
2025-07-20 15:22:53 +0530

    fix highligh bug

--- ang-prac/src/app/directives/highlight-completed-todo.ts
+++ ang-prac/src/app/directives/highlight-completed-todo.ts
@@ -1,16 +1,16 @@
-import { Directive, Input, effect, inject, ElementRef} from '@angular/core';
+import { Directive, input, effect, inject, ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appHighlightCompletedTodo]'
 })
 export class HighlightCompletedTodo {
-  @Input() isCompleted = false;
+  isCompleted = input(false);
   el = inject(ElementRef);
   
   constructor() { }
 
   styleEffects = effect(() => {
-    if(this.isCompleted){
+    if(this.isCompleted()){
       this.el.nativeElement.style.textDecoration = 'line-through';
       this.el.nativeElement.style.color = 'gray';
       this.el.nativeElement.style.backgroundColor = 'lightgray';
